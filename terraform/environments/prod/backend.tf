104e72d80aa45ddba94d234cf06bcc4bc9bb07b2
Lindrit14 <lindrit.prekaj@stud.hcw.ac.at>
2026-05-29 22:11:13 +0200

    Updated terraform ci tflint

--- terraform/environments/prod/backend.tf
+++ terraform/environments/prod/backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-avd-tfstate-weu-001"
     storage_account_name = "stavdtfstate3794ea"      # <-- dein SA_STATE
     container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    key                  = "prod.terraform.tfstate"
     use_azuread_auth     = true
   }
 }
