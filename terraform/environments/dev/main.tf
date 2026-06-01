dff349b87449a4c053a37ae51900ff07bb828773
Lindrit14 <lindrit.prekaj@stud.hcw.ac.at>
2026-06-01 23:42:39 +0200

    Resolve fileshare resource definition

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_storage_account" "storage" {
   tags                     = local.tags
 }
 
-resource "azurerm_file_share" "fileshare" {
+resource "azurerm_storage_share" "fileshare" {
   name                 = "generealfileshare"
   storage_account_name = azurerm_storage_account.storage.name
   quota                = 1
