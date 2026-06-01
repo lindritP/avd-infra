c79424ffba108a86c6bb7ce2bc9e6ea69dc91070
Lindrit14 <lindrit.prekaj@stud.hcw.ac.at>
2026-06-02 00:10:54 +0200

    Add Backup container so, file share depends on it

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -53,12 +53,21 @@ resource "azurerm_recovery_services_vault" "vault" {
   resource_group_name = azurerm_resource_group.rg-backup.name
   sku                 = local.skuName
 }
+
+
+# Register the storage account with the Recovery Services Vault
+resource "azurerm_backup_container_storage_account" "container" {
+  resource_group_name = azurerm_resource_group.rg-backup.name
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+  storage_account_id  = azurerm_storage_account.storage.id
+}
 resource "azurerm_backup_protected_file_share" "share1" {
   resource_group_name = azurerm_resource_group.rg-backup.name
   recovery_vault_name = azurerm_recovery_services_vault.vault.name
   source_file_share_name  = azurerm_storage_share.fileshare.name
   backup_policy_id    = azurerm_backup_policy_file_share.policy.id
   source_storage_account_id = azurerm_storage_account.storage.id
+
 }
 
 # Create Backup Policy for File Share
