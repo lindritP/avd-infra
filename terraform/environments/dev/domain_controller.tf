df9c0cf16a6bfdf35ade61cc11e2172cae6ab95a
Lindrit Prekaj <Lindrit_lindi@hotmail.com>
2026-06-15 12:41:28 +0200

    Fixed the fmt error

--- terraform/environments/dev/domain_controller.tf
+++ terraform/environments/dev/domain_controller.tf
@@ -75,8 +75,7 @@ resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" 
 }
 
 resource "azurerm_windows_virtual_machine" "domain_controller" {
-  name                = "dc-${local.name_suffix}"
-  computer_name       = "dc-01" 
+  name                = "dc-01"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.dc_sku
