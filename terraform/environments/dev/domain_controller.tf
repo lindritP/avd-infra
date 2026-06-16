ffb8b2e68c9c6b5dc61f00bf7511971f1bc80fa9
lindritP <lindrit@prekaj.at>
2026-06-16 09:37:44 +0200

    Added a pip to the nic of dc-01 (#3)

--- terraform/environments/dev/domain_controller.tf
+++ terraform/environments/dev/domain_controller.tf
@@ -17,12 +17,20 @@ resource "azurerm_virtual_network" "vnet" {
   dns_servers         = [local.dc_ip_address]
 }
 
-
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${local.name_suffix}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+resource "azurerm_public_ip" "domain_controller_pip" {
+  name                = "pip-${local.name_suffix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "domain_controller_nic" {
@@ -35,7 +43,13 @@ resource "azurerm_network_interface" "domain_controller_nic" {
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
     private_ip_address            = local.dc_ip_address
+    public_ip_address_id          = azurerm_public_ip.domain_controller_pip.id
   }
+
+  depends_on = [
+    azurerm_subnet.subnet,
+    azurerm_public_ip.domain_controller_pip,
+  ]
 }
 
 resource "azurerm_network_security_group" "nsg" {
