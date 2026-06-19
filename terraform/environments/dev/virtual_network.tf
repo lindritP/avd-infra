6674dd5dc7b4888d21cec9191124f6fe93e7fad9
lindritP <lindrit@prekaj.at>
2026-06-19 09:19:53 +0200

    redeplopy ansible vm now with no dns server from dc on vnet configured (#10)
    Co-authored-by: Lindrit Prekaj <Lindrit_lindi@hotmail.com>

--- terraform/environments/dev/virtual_network.tf
+++ terraform/environments/dev/virtual_network.tf
@@ -22,9 +22,9 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# resource "azurerm_subnet" "ansible" {
-#   name                 = "snet-${local.ansible_name_suffix}"
-#   resource_group_name  = azurerm_resource_group.rg-vnet.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
+resource "azurerm_subnet" "ansible" {
+  name                 = "snet-${local.ansible_name_suffix}"
+  resource_group_name  = azurerm_resource_group.rg-vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
